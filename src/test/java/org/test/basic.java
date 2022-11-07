15f1381e8ff8c2fa958b2cf0c7f1fd0ee5215be3
Gokulmani1609 <gokulmani1609@gmail.com>
2022-11-07 21:07:28 +0530

    practice

---
+++ src/test/java/org/test/basic.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class basic {
+	
+	
+@Test 
+
+private void test() {
+	System.out.println("gokul");
+	
+}
+ 
+ 
+ 
+    
+	
+	
+	
+
+}
+
